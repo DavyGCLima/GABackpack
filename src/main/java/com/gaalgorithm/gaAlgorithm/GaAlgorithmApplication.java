e6d0ef8796558102c043db09f5fed460f814e0d5
Davy Lima <davy.g.c.lima@gmail.com>
2021-05-13 23:08:20 -0300

    Start Project GA Backpack Project

---
+++ src/main/java/com/gaalgorithm/gaAlgorithm/GaAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.gaalgorithm.gaAlgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaAlgorithmApplication.class, args);
+	}
+
+}
